148aab452698f3e044ac825f4a4c76973563c1e0
yvlavrov <yvlavrov@gmail.com>
2020-08-15 13:27:05 +0300

    add sequelize and mysql2

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const path = require("path");
+const sequelize = require("./utils/database");
 const todoRoutes = require("./routes/todo");
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -11,5 +12,15 @@ app.use("/api/todo", todoRoutes);
 app.use((req, res, next) => {
   res.sendFile("/index.html");
 });
+
+async function start() {
+  try {
+    await sequelize.sync();
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+start();
 
 app.listen(PORT);
