42f7602ba69021f16f4b9b61c4cb6690c7fd0d1a
yvlavrov <yvlavrov@gmail.com>
2020-08-15 12:19:33 +0300

    init express prettier scripts

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use((req, res, next) => {
+  res.sendFile('/index.html')
+});
+
+app.listen(PORT);
